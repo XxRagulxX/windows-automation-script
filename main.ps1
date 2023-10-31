3f2e5f44d1a1112c15078b857e7354747fb77be4
DEFRO5 <52076521+DEFRO5@users.noreply.github.com>
2023-10-31 20:42:19 +0530

    Updated Indendation

--- main.ps1
+++ main.ps1
@@ -9,6 +9,7 @@ $packagesdisplay = @(
     "EpicGamesLauncher",
     "Steam",
     "qBittorrent",
+    "VLC",
     "Stremio",
     "Microsoft.Visual.c++.2015-2022.64",
     "7zip"
@@ -27,58 +28,49 @@ $packages = @(
     "EpicGames.EpicGamesLauncher",
     "Valve.Steam",
     "qBittorrent.qBittorrent",
+    "VideoLAN.VLC",
     "Stremio.Stremio",
     "Microsoft.VCRedist.2015+.x64",
     "7zip.7zip"
 )
 
-
+# Function to display package options and install
 Function InstallPackages {
-
-while ($true) {
     # Display package options
     Write-Host "Select packages to install:"
     Write-Host ("{0}. Install all packages" -f (0))
     for ($i = 0; $i -lt $packagesdisplay.Length; $i++) {
         Write-Host ("{0}. {1}" -f ($i + 1), $packagesdisplay[$i])
     }
-    Write-Host ("{0}. Exit" -f ($packagesdisplay.Length + 1))
 
-    
     # Get user input
     $selection = Read-Host "Enter the option number"
-    if ($selection -eq ($packagesdisplay.Length + 1)) {
-     Write-Host "Exiting bye ......."
-     break }
-    elseif ($selection -eq (0)) {
+    if ($selection -eq (0)) {
         # Install all packages
-        $BrowserSelection = Read-Host "What browser U need?
+        $BrowserSelection = Read-Host "What browser U need? 
         1.LibreWolf
         2.Brave
         3.Chrome"
         $confirmedbrowser = $browserpackages[$BrowserSelection - 1]
         winget install $confirmedbrowser
         foreach ($package in $packages) {
-           
-        Write-Host "Installing $selectedPackage..."
-        winget install $package --silent
-              
+            winget install $package
+        }
 
     }
     elseif ($selection -ge 1 -and $selection -le $browserpackages.Length) {
-       # Install the selected package
-       $selectedPackage = $browserpackages[$selection - 1]
-       Write-Host "Installing $selectedPackage..."
-       winget install $selectedPackage --silent
-             
+        # Install the selected browser package
+        $selectedPackage = $browserpackages[$selection - 1]
+        winget install $selectedPackage
     }
-    elseif ($selection -gt $browserpackages.Length) {
+    elseif ($selection -ge ($browserpackages.Length + 1) -and $selection -le $packages.Length) {
         # Install the selected package
-        $selectedPackage = $packages[$selection - ($browserpackages.Length + 1)]
-        Write-Host "Installing $selectedPackage..."
-        winget install $selectedPackage --silent } 
+        $selectedPackage = $packages[$selection - $browserpackages.Length - 1]
+        winget install $selectedPackage
+    } else {
+        Write-Host "Invalid selection. Please enter a valid option number."
     }
-   }
- }
+}
+
 # Call the InstallPackages function
 InstallPackages
