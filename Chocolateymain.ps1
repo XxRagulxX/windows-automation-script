c0dd6a568406f227fe5cd5f4867858801eaf309a
XxRagulxX <44401875+XxRagulxX@users.noreply.github.com>
2023-11-01 09:53:20 +0530

    Modified code with function calling

--- Chocolateymain.ps1
+++ Chocolateymain.ps1
@@ -14,6 +14,91 @@ $packages = @(
     "7zip"
 )
 
+function InstallSoftware {
+    $nextcontinue = $true
+    while ($nextcontinue) {
+        Write-Host "Select software to install:"
+        Write-Host "1. Install all software"
+
+        for ($i = 0; $i -lt $packages.Count - 3; $i++) {
+            Write-Host "$($i + 2). $($packages[$i + 3])"
+        }
+
+        $choice = Read-Host "Enter the number of the software you want to install (e.g., 1 for all, 2 for Vlc.., or 'q' to quit):"
+
+        if ($choice -eq 'q') {
+            $nextcontinue = $false
+            break
+        }
+
+        if ($choice -eq 1) {
+            foreach ($package in $packages) {
+                Write-Host "Installing $package..."
+                Invoke-Expression "choco install $package -y"
+            }
+        } elseif ($choice -ge 2) {
+            $selectedPackage = $packages[$choice - 2]
+            Write-Host "Installing $selectedPackage..."
+            Invoke-Expression "choco install $selectedPackage -y"
+        } else {
+            Write-Host "Invalid choice"
+        }
+    }
+}
+
+function InstallBrowser{
+    $browsercontinue = $true
+    while ($browsercontinue){
+        Write-Host "Select Browser to install"
+
+        for ($i = 0; $i -lt $packages.Count; $i++) {
+            Write-Host "$($i + 1). $($packages[$i])"
+            if ($i -ge 2) {
+                break
+            }
+        }
+        $choice = Read-Host "Enter the number of the Browser you want to install: "
+        if ($choice -eq 'q') {
+            $continue = $false
+            break
+        }
+        if ($choice -ge 1) {
+            $selectedPackage = $packages[$choice - 1]
+            Write-Host "Installing $selectedPackage..."
+            Invoke-Expression "choco install $selectedPackage -y"
+        } else {
+            Write-Host "Invalid choice. Please select a valid option."
+        }
+    }
+}
+
+function Uninstallsoftware{
+    $uninstallsoftware = $true
+    while ($uninstallsoftware){
+        Write-Host "Select Software to Uninstall"
+
+        for ($i = 0; $i -lt $packages.Count; $i++) {
+            Write-Host "$($i + 1). $($packages[$i])"
+        }
+        $choice = Read-Host "Enter the number of the software you want to uninstall (e.g., 1 for Brave.Brave, or 'q' to quit):"
+
+        if ($choice -eq 'q') {
+            $continue = $false
+            break
+        }
+
+        if ($choice -ge 1) {
+            $selectedPackage = $packages[$choice - 1]
+            Write-Host "Uninstalling $selectedPackage..."
+            Invoke-Expression "choco uninstall $selectedPackage -y"
+        } else {
+            Write-Host "Invalid choice. Please select a valid option."
+        }
+
+    }
+
+}
+
 $continue = $true
 
 while ($continue) {
@@ -31,77 +116,14 @@ while ($continue) {
     }
 
     if ($action -eq '1') {
-        Write-Host "Select software to install:"
-        Write-Host "1. Install all software"
-
-        for ($i = 0; $i -lt $packages.Count - 3; $i++) {
-            Write-Host "$($i + 2). $($packages[$i + 3])"
-        } #Added Proper For loop to start from VLC
-
-        $choice = Read-Host "Enter the number of the software you want to install (e.g., 1 for all, 2 for Vlc.., or 'q' to quit):" 
-
-        if ($choice -eq 'q') {
-            $continue = $false
-            continue
-        }
-
-        if ($choice -eq 1) {
-            foreach ($package in $packages) {
-                Write-Host "Installing $package..."
-                Invoke-Expression "choco install $package -y"
-            }
-        } elseif ($choice -ge 2) {
-            $selectedPackage = $packages[$choice - 2]
-            Write-Host "Installing $selectedPackage..."
-            Invoke-Expression "choco install $selectedPackage -y"
-        } else {
-            Write-Host "Invalid choice. Please select a valid option."
-        }
-    }elseif ($action -eq '2'){
-        Write-Host "Select Browser to install:"
-        for ($i = 0; $i -lt $packages.Count; $i++) {
-            Write-Host "$($i + 1). $($packages[$i])"
-            if ($i -ge 2) {
-                break
-            }
-        }
-        $choice = Read-Host "Enter the number of the Browser you want to install: "
-        if ($choice -eq 'q') {
-            $continue = $false
-            continue
-        }
-        if ($choice -ge 1) {
-            $selectedPackage = $packages[$choice - 1]
-            Write-Host "Installing $selectedPackage..."
-            Invoke-Expression "choco install $selectedPackage -y"
-        } else {
-            Write-Host "Invalid choice. Please select a valid option."
-        }
-    } else {
-        Write-Host "Invalid action. Please select a valid option."
-    }elseif ($action -eq '3') {
-        Write-Host "Select software to uninstall:"
-        for ($i = 0; $i -lt $packages.Count; $i++) {
-            Write-Host "$($i + 1). $($packages[$i])"
-        }
-
-        $choice = Read-Host "Enter the number of the software you want to uninstall (e.g., 1 for Brave.Brave, or 'q' to quit):"
-
-        if ($choice -eq 'q') {
-            $continue = $false
-            continue
-        }
-
-        if ($choice -ge 1) {
-            $selectedPackage = $packages[$choice - 1]
-            Write-Host "Uninstalling $selectedPackage..."
-            Invoke-Expression "choco uninstall $selectedPackage -y"
-        } else {
-            Write-Host "Invalid choice. Please select a valid option."
-        }
+        InstallSoftware
+    } elseif ($action -eq '2') {
+        InstallBrowser
+    } elseif ($action -eq '3') {
+        Uninstallsoftware
     } else {
         Write-Host "Invalid action. Please select a valid option."
     }
 }
 
-Write-Host "Action completed."
+Write-Host "Thank for Using. Made with Love :)"
